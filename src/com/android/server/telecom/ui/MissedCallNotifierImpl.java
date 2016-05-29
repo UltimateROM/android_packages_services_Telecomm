b166fbb4fbbacb1babed041976920e3717331fc0
Shilin Victor <chrono.monochrome@gmail.com>
2016-05-30 02:50:45 +0700

    Revert "Telecomm : Move light pulse settings to CmSettings"
    This reverts commit 6155633228083322d8eac3efb406ecfd101c7de9.
    
    Change-Id: I3db155efbf1158ce2920ee249c0eab9fcdcdf4e5

--- src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
+++ src/com/android/server/telecom/ui/MissedCallNotifierImpl.java
@@ -77,8 +77,6 @@ import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import cyanogenmod.providers.CMSettings;
-
 // TODO: Needed for move to system service: import com.android.internal.R;
 
 /**
@@ -576,19 +574,19 @@ public class MissedCallNotifierImpl extends CallsManagerListenerBase implements 
         notification.flags |= Notification.FLAG_SHOW_LIGHTS;
 
         // Get Missed call and Voice mail values if they are to be used
-        boolean customEnabled = CMSettings.System.getInt(resolver,
-                CMSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE, 0) == 1;
+        boolean customEnabled = Settings.System.getInt(resolver,
+                Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE, 0) == 1;
         if (!customEnabled) {
             notification.defaults |= Notification.DEFAULT_LIGHTS;
             return;
         }
 
-        notification.ledARGB = CMSettings.System.getInt(resolver,
-            CMSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, DEFAULT_COLOR);
-        notification.ledOnMS = CMSettings.System.getInt(resolver,
-            CMSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, DEFAULT_TIME);
-        notification.ledOffMS = CMSettings.System.getInt(resolver,
-            CMSettings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, DEFAULT_TIME);
+        notification.ledARGB = Settings.System.getInt(resolver,
+            Settings.System.NOTIFICATION_LIGHT_PULSE_CALL_COLOR, DEFAULT_COLOR);
+        notification.ledOnMS = Settings.System.getInt(resolver,
+            Settings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_ON, DEFAULT_TIME);
+        notification.ledOffMS = Settings.System.getInt(resolver,
+            Settings.System.NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF, DEFAULT_TIME);
     }
 
     private boolean canRespondViaSms(MissedCallInfo call) {
